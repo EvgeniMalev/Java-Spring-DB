d49d6c45d74be618c75a89e0b701cbdfb098a30a
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-04-25 22:43:09 +0300

    Create car.java

---
+++ car.java
@@ -0,0 +1,37 @@
+public class Car {
+    private int id;
+    private String name;
+    private String model;
+    private int year;
+    private double engine;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+       this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+       this.model = model;
+    }  
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+       this.year = year;
+    }  
+      public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+       this.engine = engine;
+    }   
+}
