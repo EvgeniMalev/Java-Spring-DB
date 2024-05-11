22458e3145bec96744f0673209c095611786e624
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-05-11 11:34:54 +0300

    Create CarService.java

---
+++ Hibernate/Cars/src/main/java/com/example/cars/services/CarService.java
@@ -0,0 +1,20 @@
+package com.example.demo.services;
+
+import com.example.demo.models.Car;
+import com.example.demo.repositories.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarService {
+
+    private final CarRepository carRepository;
+
+    @Autowired
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+}
