da04b12b178f45e92854e93a666a1e57cbebf80a
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-05-11 11:50:19 +0300

    Update data.sql

--- Hibernate/Cars/src/main/resources/data.sql
+++ Hibernate/Cars/src/main/resources/data.sql
@@ -2,5 +2,6 @@ INSERT INTO car (make, model) VALUES ('Trabant', '601 Standard', 'V 6601 VV');
 INSERT INTO car (make, model) VALUES ('Trabant', '601 Limosine', 'VT 8888 EV');
 INSERT INTO car (make, model) VALUES ('Trabant', '601 S', 'E 2134 BE');
 INSERT INTO car (make, model) VALUES ('Trabant', '601 Hycoma', 'R 1960 TT');
+INSERT INTO car (make, model) VALUES ('Trabant', '601 Hycoma', 'VR 1966 TE');
 
 INSERT INTO car (make, model) VALUES ('Trabant', '600', 'SV 90 VV');
